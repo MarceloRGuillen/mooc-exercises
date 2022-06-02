734e9c5776ac267d4f174daacfebce3de786aeb5
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-02 18:31:33 -0400

    Complete exercise 19

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,15 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Give a number:");
+        int end = Integer.valueOf(scanner.nextLine());
+        int factorial = 1;
+        if(end == 0) {
+            System.out.println("Factorial: 1");
+        }
+        for(int i = 1; i <= end; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial: "+factorial);
     }
 }
