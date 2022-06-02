e6a6d9552539464a872328e2ba9d7e5bcdd48314
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-02 14:44:17 -0400

    Complete exercise 11

--- mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int counter = 0;
+        while(true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if(num == 0) {
+                break;
+            }
+            sum = sum + num;
+            counter++;
+        }
+        System.out.println("Number of numbers: "+counter);
+        System.out.println("Sum of the numbers: "+sum);
     }
 }
