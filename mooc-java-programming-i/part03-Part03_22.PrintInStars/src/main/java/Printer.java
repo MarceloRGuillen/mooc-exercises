5d5bd63861cbe07e513065788b2b908c01a9859f
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-16 19:45:48 -0400

    Complete part 3 exercise 22

--- mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -9,6 +9,12 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
+        for(int i = 0; i < array.length; i++) {
+            for(int x = 0; x < array[i]; x++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
 }
