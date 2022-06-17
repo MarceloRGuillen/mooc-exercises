74581d68eae622ed8c7bfd64c8b26fb732530b84
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-17 14:32:28 -0400

    Complete part 3 exercise 26

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,15 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while(true) {
+            String words = scanner.nextLine();
+            if(words.equals("")) {
+                break;
+            }
+            String[] pieces = words.split(" ");
+            for(int i = 0; i < pieces.length; i++) {
+                System.out.println(pieces[i]);
+            }
+        }
     }
 }
