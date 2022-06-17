7e66398305a405a2368d2c68e4b2db1ef0d28d01
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-17 15:29:26 -0400

    Complete part 3 exercise 31

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,21 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String[] pieces;
+        int oldest = 0;
+        String name = "";
+        
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("")) {
+                break;
+            }
+             pieces = input.split(",");
+             if(Integer.valueOf(pieces[1]) > oldest) {
+                 oldest = Integer.valueOf(pieces[1]);
+                 name = pieces[0];
+             }
+        }
+        System.out.println("Name of the oldest: "+name);
     }
 }
