e98414c183f1481e003b9608a0b53db7a8f1d88c
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-17 15:26:38 -0400

    Complete part 3 exercise 30

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,19 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String[] pieces;
+        int oldest = 0;
+        
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("")) {
+                break;
+            }
+             pieces = input.split(",");
+             if(Integer.valueOf(pieces[1]) > oldest) {
+                 oldest = Integer.valueOf(pieces[1]);
+             }
+        }
+        System.out.println("Age of the oldest: "+oldest);
     }
 }
