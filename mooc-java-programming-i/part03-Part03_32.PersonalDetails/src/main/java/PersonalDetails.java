2a0cead79d8e4e990748c831377f720455652b65
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-17 15:45:27 -0400

    Complete part 3 exercise 32

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,28 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String[] pieces;
+        int longest = 0;
+        double avg = 0.0;
+        int sum = 0;
+        int counter = 0;
+        String name = "";
+        
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("")) {
+                break;
+            }
+             pieces = input.split(",");
+            if(pieces[0].length() > longest) {
+                longest = pieces[0].length();
+                name = pieces[0];
+            }
+            sum += Integer.valueOf(pieces[1]);
+            counter++;
+        }
+        System.out.println("Longest name: "+name);
+        avg = (sum * 1.0) / counter;
+        System.out.println("Average of the birth years: "+avg);
     }
 }
