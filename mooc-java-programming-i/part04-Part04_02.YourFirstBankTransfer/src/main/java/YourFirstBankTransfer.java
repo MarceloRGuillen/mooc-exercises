aea2cb689f07a9ab49df746f8415541d7c7f5304
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-20 12:54:10 -0400

    Complete part 4 exercise 2

--- mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account checking = new Account("Matthews account", 1000);
+        Account checking2 = new Account("My account", 0);
+        checking.withdrawal(100.0);
+        checking2.deposit(100.0);
+        checking.toString();
+        checking2.toString();
     }
 }
