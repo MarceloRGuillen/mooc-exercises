ee983f0a2e59b081fb1c7730020aa57b7118cc6f
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-20 12:52:54 -0400

    Complete part 4 exercise 1

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account money = new Account("Marcelo", 100.00);
+        money.deposit(20.00);
+        money.toString();
     }
 }
