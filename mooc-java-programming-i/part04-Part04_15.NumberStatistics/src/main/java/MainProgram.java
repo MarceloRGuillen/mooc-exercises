ce786a73fd58bb7d2b02a5f115852cb6a23d3a13
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-21 12:49:48 -0400

    Complete part 4 exercise 15

--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,31 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        
+        Statistics statistics = new Statistics();
+        Statistics evenStatistics = new Statistics();
+        Statistics oddStatistics = new Statistics();
+        
+        System.out.println("Enter numbers:");
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input != -1)
+            {
+                if(input%2 == 0) {
+                    evenStatistics.addNumber(input);
+                } else {
+                    oddStatistics.addNumber(input);
+                }
+                statistics.addNumber(input);
+            } else {
+                break;
+            }
+        }
+        
+        System.out.println("Sum: "+statistics.sum());
+        System.out.println("Sum of even numbers: "+evenStatistics.sum());
+        System.out.println("Sum of odd numbers: "+oddStatistics.sum());
     }
 }
