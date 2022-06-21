38ad85b012777219eb89ba52fb641ddb423c09c6
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-21 15:37:56 -0400

    Complete part 4 exercise 20

--- mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        ArrayList<Book> books = new ArrayList<Book>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("Title: ");
+            String title = scanner.nextLine();
+            if(title.equals("")) {
+                break;
+            }
+            System.out.println("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            System.out.println("Publication year: ");
+            int publicationYear = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(title, pages, publicationYear));
+        }
+        
+        System.out.println("What information will be printed? ");
+        String input = scanner.nextLine();
+        if(input.equals("everything")) {
+            for(Book i: books) {
+                System.out.println(i);
+            } 
+        } else {
+            for(Book i: books) {
+                System.out.println(i.getTitle());
+            }
+        }
     }
 }
