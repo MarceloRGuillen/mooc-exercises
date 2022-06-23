14c7831be5b2257e4d0e41d1023230d6024b6130
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-23 15:24:28 -0400

    Complete part 4 exercise 25

--- mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,20 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String name = scanner.nextLine();
+        try (Scanner file = new Scanner(Paths.get(name))) {
 
+        // we read the file until all lines have been read
+        while (file.hasNextLine()) {
+            // we read one line
+            String row = file.nextLine();
+            // we print the line that we read
+            System.out.println(row);
+        }
+    } catch (Exception e) {
+        System.out.println("Error: " + e.getMessage());
+    }
+        
     }
 }
