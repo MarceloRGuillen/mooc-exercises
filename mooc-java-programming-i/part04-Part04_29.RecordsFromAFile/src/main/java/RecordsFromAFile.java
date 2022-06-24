bc087eeca03164ad94471ce27ed57aaa0864857a
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-06-23 20:09:33 -0400

    Complete part 4 exercise 29

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,22 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+        try(Scanner data = new Scanner(Paths.get(file))) {
+            while(data.hasNextLine()) {
+             String line = data.nextLine();
+             String[] pieces = line.split(",");
+             String name = pieces[0];
+             int age = Integer.valueOf(pieces[1]);
+             if(age == 1) {
+                 System.out.println(name+", age: "+age+" year");
+             } else {
+                System.out.println(name+", age: "+age+" years");
+             }
+            }
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
