d8ffce085a5880abfaaa720b70322f33d9e545d9
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-07-08 15:02:43 -0400

    Complete part 5 exercise 9

--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,20 @@
 
 public class HealthStation {
 
+    private int weighingCounter = 0;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.weighingCounter++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.weighingCounter;
     }
 
 }
