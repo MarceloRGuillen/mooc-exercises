8a9f3ea28f13ba9d992d5448a6c89e72812c0023
Marcelo Guillen <marceloguillenbro@gmail.com>
2022-08-18 21:07:30 -0400

    Complete part 5 exercise 14

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,21 @@ public class Book {
         return publicationYear;
     }
 
+    public boolean equals(Object compared) {
+        if(this == compared) {
+            return true;
+        }
+        
+        if(!(compared instanceof Book)) {
+            return false;
+        }
+        
+        Book test = (Book) compared;
+        
+        if(this.getName().equals(test.getName()) && this.publicationYear == test.publicationYear) {
+            return true;
+        }
+        return false;
+    }
+    
 }
--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
         ArrayList<Book> books = new ArrayList<>();
 
         while (true) {
+            boolean found = false;
             System.out.println("Name (empty will stop):");
             String name = scanner.nextLine();
             if (name.isEmpty()) {
@@ -18,7 +19,23 @@ public class Main {
             System.out.println("Publication year:");
             int publicationYear = Integer.valueOf(scanner.nextLine());
             Book book = new Book(name, publicationYear);
-            books.add(book);
+            
+            if(books.size() == 0) {
+                books.add(book);
+            }
+            else {
+                for(int i = 0; i < books.size(); i++)
+                {
+                    if(books.get(i).equals(book)) {
+                        System.out.println("The book is already on the list. Let's not add the same book again."); 
+                        found = true;
+                        break;
+                    }
+                }
+                if(found == false) {
+                    books.add(book);   
+                }
+            }
 
         }
         
